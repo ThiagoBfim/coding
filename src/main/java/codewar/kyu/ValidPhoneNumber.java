49d8b285f4079af81a37223d9b8dc6bd8ec04a3c
Thiago Bomfim <thiagobomfim1995@gmail.com>
2022-03-15 08:36:32 +0000

    Include PhoneNumber Regex exercise

---
+++ src/main/java/codewar/kyu/ValidPhoneNumber.java
@@ -0,0 +1,19 @@
+package codewar.kyu;
+
+import java.util.regex.Pattern;
+
+/**
+ * Write a function that accepts a string, and returns true if it is in the form of a phone number.
+ * Assume that any integer from 0-9 in any of the spots will produce a valid phone number.
+ *
+ * Only worry about the following format:
+ * (123) 456-7890 (don't forget the space after the close parentheses)
+ */
+public class ValidPhoneNumber {
+
+    public static boolean validPhoneNumber(String phoneNumber) {
+        var pattern = Pattern.compile("^\\(\\d{3}\\) \\d{3}\\-\\d{4}$");
+        var matcher = pattern.matcher(phoneNumber);
+        return matcher.find();
+    }
+}
---
+++ src/test/java/codewar/kyu/ValidPhoneNumberTest.java
@@ -0,0 +1,17 @@
+package codewar.kyu;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ValidPhoneNumberTest {
+
+    @org.junit.jupiter.api.Test
+    void validPhoneNumber() {
+        assertEquals(true, ValidPhoneNumber.validPhoneNumber("(123) 456-7890"));
+        assertEquals(false, ValidPhoneNumber.validPhoneNumber("(1111)555 2345"));
+        assertEquals(false, ValidPhoneNumber.validPhoneNumber("(098) 123 4567"));
+        assertEquals(false, ValidPhoneNumber.validPhoneNumber("(098a) 123-4567"));
+        assertEquals(false, ValidPhoneNumber.validPhoneNumber("a(098) 123-4567"));
+        assertEquals(false, ValidPhoneNumber.validPhoneNumber("(098) 123-4567a"));
+        assertEquals(false, ValidPhoneNumber.validPhoneNumber("(098) 121*-4567"));
+    }
+}
