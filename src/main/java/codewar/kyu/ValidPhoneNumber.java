6673e37a135561f83e48c5529e872ed3cbe98f05
Thiago Bomfim <thiagobomfim1995@gmail.com>
2022-03-15 08:38:36 +0000

    Include a easy way

--- src/main/java/codewar/kyu/ValidPhoneNumber.java
+++ src/main/java/codewar/kyu/ValidPhoneNumber.java
@@ -16,4 +16,8 @@ public class ValidPhoneNumber {
         var matcher = pattern.matcher(phoneNumber);
         return matcher.find();
     }
+
+    public static boolean betterSolution(String phoneNumber) {
+        return phoneNumber.matches("\\(\\d{3}\\) \\d{3}\\-\\d{4}");
+    }
 }
