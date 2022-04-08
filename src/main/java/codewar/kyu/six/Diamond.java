f1580858865dda2a96cd71634da342aa9ee08309
Thiago Bomfim <thiagobomfim1995@gmail.com>
2022-04-08 07:52:17 +0100

    Including the Diamond Challenge

---
+++ src/main/java/codewar/kyu/six/Diamond.java
@@ -0,0 +1,38 @@
+package codewar.kyu.six;
+
+
+public class Diamond {
+
+    public static String print(int n) {
+        if (n <= 0 || n % 2 == 0) {
+            return null;
+        }
+
+        var diamond = new StringBuilder();
+        int spaces = n / 2;
+        for (int i = 1; i <= n; i += 2) {
+            diamond.append(sumCharacters(spaces, " "))
+                    .append(sumCharacters(i, "*"))
+                    .append("\n");
+            spaces--;
+        }
+        var lastSpace = 1;
+        for (int i = n - 2; i >= 1; i -= 2) {
+            diamond.append(sumCharacters(lastSpace, " "))
+                    .append(sumCharacters(i, "*"))
+                    .append("\n");
+            lastSpace++;
+        }
+
+        return diamond.toString();
+    }
+
+
+    private static String sumCharacters(int numberSpaces, String character) {
+        if (numberSpaces <= 0) {
+            return "";
+        }
+        return new String(new char[numberSpaces]).replace("\0", character);
+    }
+
+}
