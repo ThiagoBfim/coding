53d84f995af8863f4562c3456cd89c03ba1b5321
Thiago Bomfim <thiagobomfim1995@gmail.com>
2022-04-12 07:49:51 +0100

    Including a Clever solution

--- src/main/java/codewar/kyu/six/RomanEncoder.java
+++ src/main/java/codewar/kyu/six/RomanEncoder.java
@@ -1,11 +1,40 @@
 package codewar.kyu.six;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class RomanEncoder {
+    private static TreeMap<Integer, String> MAP;
 
+    static {
+        MAP = new TreeMap<>(Collections.reverseOrder());
+        MAP.put( 1, "I" );
+        MAP.put( 4, "IV" );
+        MAP.put( 5, "V" );
+        MAP.put( 9, "IX" );
+        MAP.put( 10, "X" );
+        MAP.put( 40, "XL" );
+        MAP.put( 50, "L" );
+        MAP.put( 90, "XC" );
+        MAP.put( 100, "C" );
+        MAP.put( 400, "CD" );
+        MAP.put( 500, "D" );
+        MAP.put( 900, "CM" );
+        MAP.put( 1000, "M" );
+    }
+
+    public String solutionClever(int n) {
+        var builder = new StringBuilder();
+        for( Map.Entry<Integer, String> entry: MAP.entrySet() ){
+            int i = entry.getKey();
+            String s = entry.getValue();
+            while( n>=i ){
+                builder.append(s);
+                n -= i;
+            }
+        }
+        return builder.toString();
+    }
 
     public String solution(int value) {
         var buildNumber = new StringBuilder();
