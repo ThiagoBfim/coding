eab99e07d43cd27600b7d941aba04e991d5884db
Thiago Bomfim <thiagobomfim1995@gmail.com>
2022-04-25 07:52:32 +0100

    Including more unit test

--- src/test/java/codewar/kyu/four/CountingChangeCombinationsTest.java
+++ src/test/java/codewar/kyu/four/CountingChangeCombinationsTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class CountingChangeCombinationsTest {
 
@@ -15,5 +17,50 @@ class CountingChangeCombinationsTest {
         Assertions.assertThat(CountingChangeCombinations.count(11, Arrays.asList(5, 7))).isZero();
         Assertions.assertThat(CountingChangeCombinations.count(11, Arrays.asList(5, 1))).isEqualTo(3);
         Assertions.assertThat(CountingChangeCombinations.count(12, Arrays.asList(1, 2, 3))).isEqualTo(19);
+    }
+
+    @Test
+    public void testSimple() {
+        assertEquals(3, CountingChangeCombinations.count(4, Arrays.asList(1, 2)));
+    }
+
+    @Test
+    public void testAnother() {
+        assertEquals(4, CountingChangeCombinations.count(10, Arrays.asList(5,2,3)));
+    }
+
+    @Test
+    public void testNoChange() {
+        assertEquals(0, CountingChangeCombinations.count(11, Arrays.asList(5,7)));
+    }
+
+    @Test
+    public void test1() {
+        assertEquals(1022, CountingChangeCombinations.count(300, Arrays.asList(5,10,20,50,100,200,500)));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals(1022, CountingChangeCombinations.count(300, Arrays.asList(500,5,50,100,20,200,10)));
+    }
+
+    @Test
+    public void test3() {
+        assertEquals(0, CountingChangeCombinations.count(301, Arrays.asList(5,10,20,50,100,200,500)));
+    }
+
+    @Test
+    public void test4() {
+        assertEquals(760, CountingChangeCombinations.count(199, Arrays.asList(3,5,9,15)));
+    }
+
+    @Test
+    public void test5() {
+        assertEquals(19, CountingChangeCombinations.count(98, Arrays.asList(3,14,8)));
+    }
+
+    @Test
+    public void test6() {
+        assertEquals(18515, CountingChangeCombinations.count(419, Arrays.asList(2,5,10,20,50)));
     }
 }
