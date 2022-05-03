f6284b1c9c23fbc4d24dbd4b9f78af016349d2fd
Thiago Bomfim <thiagobomfim1995@gmail.com>
2022-05-03 08:16:01 +0100

    Including a Clever Solution

--- src/main/java/codewar/kyu/five/FactDecomp.java
+++ src/main/java/codewar/kyu/five/FactDecomp.java
@@ -61,4 +61,24 @@ public class FactDecomp {
         }
     }
 
+    public static String decompClever(int n) {
+        int[] exponentsOfPrimes = new int[n+1];
+        while (n>1) {
+            int x = n--;
+            for (int i=2; i<=Math.sqrt(x); i++)
+                if (x % i == 0) {
+                    x /= i;
+                    exponentsOfPrimes[i]++;
+                    i = 1;
+                }
+            exponentsOfPrimes[x]++;
+        }
+        StringBuilder result = new StringBuilder();
+        for (int i = 2; i < exponentsOfPrimes.length; i++) {
+            if (exponentsOfPrimes[i] == 0) continue;
+            if (exponentsOfPrimes[i] == 1) result.append(i + " * ");
+            if (exponentsOfPrimes[i] >  1) result.append(i + "^" + exponentsOfPrimes[i] + " * ");
+        }
+        return result.substring(0,result.length()-3);
+    }
 }
